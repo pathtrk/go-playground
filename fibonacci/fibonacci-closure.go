11e1c7422947cd415b5486bca2fec9c71439919f
Ryosuke Goto <ryosuke.goto@gmail.com>
2018-01-23 18:08:41 +0900

    update the comment

--- fibonacci/fibonacci-closure.go
+++ fibonacci/fibonacci-closure.go
@@ -2,8 +2,7 @@ package main
 
 import "fmt"
 
-// fibonacci is a function that returns
-// a function that returns an int.
+// fibonacci returns a function that calcurates the series
 func fibonacci() func() int {
 	n0, n1 := 0, 1
 	return func() int {
